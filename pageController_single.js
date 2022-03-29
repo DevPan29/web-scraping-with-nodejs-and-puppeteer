23a02c62676cf3fb1bbe05e0237aae54a34342ae
Alepan <darenagel@hotmail.it>
2022-03-29 19:13:46 +0200

    step 5: scraping by category

--- pageController_single.js
+++ pageController_single.js
@@ -2,15 +2,15 @@ const pageScraper = require('./pageScraper');
 async function scrapeAll(browserInstance){
 	let browser;
 	try{
+        /* this browserInstance is a Promise and we should use then/catch or await to resolve it
+        */
 		browser = await browserInstance;
-		let scrapedData = {};
-		// Call the scraper for different set of books to be scraped
-		scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
-		await browser.close();
-		console.log(scrapedData)
+		await pageScraper.scraper(browser);	
+		
 	}
 	catch(err){
 		console.log("Could not resolve the browser instance => ", err);
 	}
 }
+
 module.exports = (browserInstance) => scrapeAll(browserInstance)
